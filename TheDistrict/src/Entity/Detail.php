96f1022b3e119041177a4e0f001154a320b0b19e
Jeoffrey80 <meyxana80@gmail.com>
2023-11-07 09:04:48 +0100

    création des tables + bdd + liaison

---
+++ TheDistrict/src/Entity/Detail.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DetailRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DetailRepository::class)]
+class Detail
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    #[ORM\ManyToOne(inversedBy: 'details')]
+    private ?Plat $Plat = null;
+
+    #[ORM\ManyToOne(inversedBy: 'details')]
+    private ?Commande $Commande = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getPlat(): ?Plat
+    {
+        return $this->Plat;
+    }
+
+    public function setPlat(?Plat $Plat): static
+    {
+        $this->Plat = $Plat;
+
+        return $this;
+    }
+
+    public function getCommande(): ?Commande
+    {
+        return $this->Commande;
+    }
+
+    public function setCommande(?Commande $Commande): static
+    {
+        $this->Commande = $Commande;
+
+        return $this;
+    }
+}
