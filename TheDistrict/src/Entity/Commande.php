73281e1ddfd77fbe48b3939a1c5e6e95d13332bc
Jeoffrey80 <meyxana80@gmail.com>
2023-11-24 09:26:07 +0100

    MàJ+Test avec Restfull +API

--- TheDistrict/src/Entity/Commande.php
+++ TheDistrict/src/Entity/Commande.php
@@ -6,11 +6,11 @@ use App\Repository\CommandeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Metadata\ApiResource;
+// use ApiPlatform\Metadata\ApiResource;
 
 
 
-#[ApiResource]
+// #[ApiResource]
 #[ORM\Entity(repositoryClass: CommandeRepository::class)]
 class Commande
 {
